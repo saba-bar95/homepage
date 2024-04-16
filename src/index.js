4ef0d2060752df7bba6c420bc4f4410d8a314afa
saba-bar95 <s.barbakadze95@gmail.com>
2024-04-16 17:04:31 +0400

    Add tablet design

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import "./index.scss";
 import createProjects from "./components/projects/createProject";
 
 createProjects();
+
+const landmarks = document.querySelectorAll(".landmark");
+const projects = document.querySelectorAll(".project");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+landmarks.forEach((item) => {
+  observer.observe(item);
+});
+
+projects.forEach((item) => {
+  observer.observe(item);
+});
